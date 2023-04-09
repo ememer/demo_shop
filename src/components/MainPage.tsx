25e55642809feca65afee92d7795a2b5b5a5a61a
ememer <46853050+ememer@users.noreply.github.com>
2023-04-09 12:58:11 +0200

    Hero images connect to query, fix sizing

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
+import { gql, useQuery } from '@apollo/react-hooks';
 
-import { MainQuery } from '../types/MainQuery';
+import { ProductStoresQuery } from '../types/ProductsStores';
+import { SITE_URL } from '../utils/siteUrl';
 
 import BenefitsSection from './BenefitsSection';
 import BestSeller from './BestSeller';
@@ -11,44 +11,46 @@ import BestSeller from './BestSeller';
 import '../sass/custom/MainPage.scss';
 
 const MainPage = () => {
-  const { data } = useQuery(MAIN_PAGE_QUERY);
-
-  const products = (data?.products as MainQuery['products'])?.data[0]?.attributes ?? [];
-
+  const { data } = useQuery(PRODUCT_STORES);
+  const stores = (data as ProductStoresQuery)?.productsStores?.data[0]?.attributes ?? [];
+  const leftColumn = stores?.leftImage;
+  const rightColumn = stores?.rightImage;
   return (
     <>
       <div
         style={{ marginTop: '10rem', height: 'auto' }}
         className="row justify-content-center"
       >
+        {/* LEFT COLUMN */}
         <div className="col-6 p-0 pe-1">
-          <button className="border-0 hover p-0 m-0 position-relative bg-transparent ">
+          <button className="border-0 hover p-0 m-0 position-relative bg-transparent h-100">
             <img
-              alt="randomImg"
-              src="https://picsum.photos/seed/picsum/1200/1300"
-              className="img-fluid d-block mx-auto rounded-4 shadow"
+              alt="Kategoria obudowy iPhone"
+              src={`${SITE_URL}${leftColumn?.image?.data?.attributes?.url}`}
+              className="img-fluid img-thumbnail d-block mx-auto rounded-4 shadow h-100"
             />
             <div className="hover-overlay rounded-4" />
             <h5 className="col-12 position-absolute top-50 text-center text-white fw-bold fs-1">
-              Some random text here
+              {leftColumn?.title}
             </h5>
           </button>
         </div>
+        {/* RIGHT COLUMN */}
         <div className="col-6 p-0 ps-1">
-          <button className="border-0 hover p-0 m-0 position-relative bg-transparent ">
+          <button className="border-0 hover p-0 m-0 position-relative bg-transparent h-100">
             <img
-              alt="randomImg"
-              src="https://picsum.photos/seed/picsum/1200/1300"
-              className="img-fluid d-block mx-auto rounded-4 shadow"
+              alt="Kategoria opaski na zegarek iWatch"
+              src={`${SITE_URL}${rightColumn?.image?.data?.attributes?.url}`}
+              className="img-fluid img-thumbnail d-block mx-auto rounded-4 shadow h-100"
             />
             <div className="hover-overlay rounded-4" />
             <h5 className="col-12 position-absolute top-50 text-center text-white fw-bold fs-1">
-              Some random text here
+              {rightColumn?.title}
             </h5>
           </button>
         </div>
       </div>
-      <BestSeller products={products} />
+      <BestSeller />
       <BenefitsSection />
     </>
   );
@@ -56,39 +58,27 @@ const MainPage = () => {
 
 export default MainPage;
 
-const MAIN_PAGE_QUERY = gql`
-  query MainPage {
-    products {
+const PRODUCT_STORES = gql`
+  query ProductsStores {
+    productsStores {
       data {
         attributes {
-          name
-          price
-          model_products {
-            data {
-              attributes {
-                product_model
+          leftImage {
+            title
+            image {
+              data {
+                attributes {
+                  url
+                }
               }
             }
           }
-          product_images {
-            data {
-              attributes {
-                url
-                alternativeText
-                formats
-              }
-            }
-          }
-          product_configurations {
-            data {
-              attributes {
-                configuration
-                configuration_picture {
-                  data {
-                    attributes {
-                      formats
-                    }
-                  }
+          rightImage {
+            title
+            image {
+              data {
+                attributes {
+                  url
                 }
               }
             }
